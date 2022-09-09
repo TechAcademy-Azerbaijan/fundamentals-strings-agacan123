b84efc08d5ecd41958bd48528c267f5dc5507537
agacan123 <110633844+agacan123@users.noreply.github.com>
2022-09-09 11:44:23 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -3,7 +3,18 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
+   let num = result.input
+  let first = num[0]
+  let last = num[2]
+
+  if (first > last){
+    console.log(first);
   
-    // Write code here
+  } else if (last>first){
+    console.log(last);
+  }else{
+    console.log('=');
+  }
   
+
 });
