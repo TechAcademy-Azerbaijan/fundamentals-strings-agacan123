7785cb66db595488ae10151a0990b645d9197a7a
agacan123 <110633844+agacan123@users.noreply.github.com>
2022-09-09 11:46:48 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -3,7 +3,20 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
+  let n = result.input
+   let count2 = 0
+   let count5 = 0
+   for (let i = 0; i < n.length; i++){
+    const element = n[i]
+    if (element==2){
+count2++
+    }
+   }if (element==5){
+    count5++
+
+   }
+   
+  console.log(count);
+ 
   
 });
