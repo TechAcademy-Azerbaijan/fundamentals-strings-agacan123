594c4265af3696c91cedc3db83fcd0fc36aa4d26
agacan123 <110633844+agacan123@users.noreply.github.com>
2022-09-09 11:42:35 +0400

    Update main.js

--- task-6/main.js
+++ task-6/main.js
@@ -3,7 +3,19 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
+  let n = result.input
+  let count = 0
+  for (let i = 0; i < n.length; i++) {
+    const element = n[i];
+   if ((element ==  " " || element=="!"  || element == "," )&& n[i+1]!== " ") {
+    count++
+    
+   }
+        
+    
+  }
+    console.log(count);
   
-    // Write code here
+
   
 });
