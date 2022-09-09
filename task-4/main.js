ddddd18724fbf25f3c08c851213bda4acfff341a
agacan123 <110633844+agacan123@users.noreply.github.com>
2022-09-09 11:49:41 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -3,7 +3,13 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
+   let n = result.input
+    let count = 0
+    for(i = 0; i < n.length; i++){
+if (n[i]== "," || n [i] == "!")
+count++
+    }
+  console.log(count);
   
-    // Write code here
   
 });
